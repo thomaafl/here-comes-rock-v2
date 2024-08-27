b908b0cb04f9d1dcf85ab58de79d0125f4fda248
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-08-27 08:45:39 +0200

    had 2 do it all over again

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # here-comes-rock-v2
+
+
+The better version of "rock - paper - scissors"
+With powerups!
---
+++ rockgame.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace rockgame.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
